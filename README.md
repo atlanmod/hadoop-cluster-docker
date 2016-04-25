f0dad7ed779eb4b26a7f3bbc937dade34538eeda
Amine Benelallam <amine.benallam@gmail.com>
2016-04-25 15:58:34 +0200

    adding configuration files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Credits
+Scripts adapted from  [kiwenlau/hadoop-cluster-docker](https://github.com/kiwenlau/hadoop-cluster-docker). 
+Informations about how to use this repository will be provided soon
+**N.B**: This fork contains data related to an experiment.
+
+# Organization 
+
+# Content and description 
+
+# How to use
---
+++ start-container.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# run N slave containers
+N=$1
+
+# the defaut node number is 3
+if [ $# = 0 ]
+then
+	N=3
+fi
+	
+
+# delete old master container and start new master container
+docker rm -f master &> /dev/null
+echo "start master container..."
+docker run -d -t --dns 127.0.0.1 -P  --name master -h master.mondo.com -w /root amineben/hadoop-master:0.2.0 
+# get the IP address of master container
+FIRST_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" master)
+
+# delete old slave containers and start new slave containers
+i=1
+while [ $i -lt $N ]
+do
+	docker rm -f slave$i &> /dev/null
+	echo "start slave$i container..."
+	docker run -d -t --dns 127.0.0.1 -P  --name slave$i -h slave$i.mondo.com -e JOIN_IP=$FIRST_IP amineben/hadoop-slave:0.2.0 
+	((i++))
+done 
+
+# create a new Bash session in the master container
+docker exec -it master bash
