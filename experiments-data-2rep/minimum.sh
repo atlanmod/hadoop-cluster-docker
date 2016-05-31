a02524a0ad8eb945efc34cbfda3c75dec0ff749f
Amine BENELALLAM <amine.benallam@gmail.com>
2016-05-31 21:30:32 +0000

    adding experiment data

---
+++ experiments-data-2rep/minimum.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+declare -a sizeModels=( '10000' '20000' )
+
+for model in ${!sizeModels[*]}
+do
+  i=${sizeModels[$model]}
+  cat timings-dag-$i-cut.txt | awk '{ min=int($2); for(i=3;i<=NF;i++) {  min = (min < int($i)) ? min : int($i) }; print $1,"\t", int(min) }' &> timings-dag-$i-min.txt
+done
